1032d8fb82ee5919375d0b131df20a47cabfe717
pulsely <opensource@pulsely.com>
2023-07-22 11:07:34 +0100

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext, gettext_lazy as _
+from . import models
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
